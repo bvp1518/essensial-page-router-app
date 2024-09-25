a7c5a74b6d757c1aa0f0d6cf1e4c1d89392b8e95
bvp1518 <bvp18061992@gmail.com>
2024-09-25 17:33:21 +0530

    till-469. The "_app.js" File & Layout Wrapper

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,26 @@
 import MeetupList from "@/components/meetups/MeetupList";
 
-const DUMMY_MEETUPS =[
+
+const DUMMY_MEETUPS = [
     {
-    id: 'm1',
-    title: 'A First Meetup',
-    image: 'https://png.pngtree.com/thumb_back/fh260/background/20210907/pngtree-business-daytime-conference-table-meeting-room-placement-photographs-and-pictures-image_816353.jpg',
-    address: 'Some address 5, 12345 some city',
-    description: 'This is a first meetup!'
-},
-{
-    id: 'm2',
-    title: 'A second Meetup',
-    image: 'https://png.pngtree.com/thumb_back/fh260/background/20210907/pngtree-business-daytime-conference-table-meeting-room-placement-photographs-and-pictures-image_816353.jpg',
-    address: 'Some address 5, 12345 some city',
-    description: 'This is a second meetup!'
-}
+        id: 'm1',
+        title: 'A First Meetup',
+        image: 'https://png.pngtree.com/thumb_back/fh260/background/20210907/pngtree-business-daytime-conference-table-meeting-room-placement-photographs-and-pictures-image_816353.jpg',
+        address: 'Some address 5, 12345 some city',
+        description: 'This is a first meetup!'
+    },
+    {
+        id: 'm2',
+        title: 'A second Meetup',
+        image: 'https://png.pngtree.com/thumb_back/fh260/background/20210907/pngtree-business-daytime-conference-table-meeting-room-placement-photographs-and-pictures-image_816353.jpg',
+        address: 'Some address 5, 12345 some city',
+        description: 'This is a second meetup!'
+    }
 ];
 
 function HomePage() {
     return <MeetupList meetups={DUMMY_MEETUPS} />
+    
 }
 
 export default HomePage;
