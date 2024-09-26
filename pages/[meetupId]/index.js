519f9df8ff9fab944485c3a70ca37d77f19abd90
bvp1518 <bvp18061992@gmail.com>
2024-09-26 15:45:59 +0530

    till-482. Getting Meetup Details Data & Preparing Pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,50 +1,58 @@
+import { MongoClient, ObjectId } from "mongodb";
+
 import MeetupDetail from "@/components/meetups/MeetupDetail";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
 
    return (
       <MeetupDetail
-         image='https://png.pngtree.com/thumb_back/fh260/background/20210907/pngtree-business-daytime-conference-table-meeting-room-placement-photographs-and-pictures-image_816353.jpg'
-         title='First Meetup'
-         address='Some Street 5, Some City'
-         description='This is a first meetup'
+         image={props.meetupData.image}
+         title={props.meetupData.title}
+         address={props.meetupData.address}
+         description={props.meetupData.description}
       />
    );
 }
 
 export async function getStaticPaths() {
+   const client = await MongoClient.connect('mongodb+srv://bvp18061992:DGfa3uKmiU4SCt5m@cluster0.j5ygu.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0');
+   const db = client.db();
+
+   const meetupsCollection = db.collection('meetups');
+   const meetups = await meetupsCollection.find({}, {_id:1}).toArray();
+  
+   client.close();
+
    return {
       fallback: false,
-      paths: [
-         {
-            params: {
-               meetupId: 'm1',
-            },
-         },
-         {
-            params: {
-               meetupId: 'm2',
-            },
-         }
-      ]
+      paths: meetups.map(meetup => ({
+         params:{meetupId: meetup._id.toString()}
+      })),
    }
 }
-
-export async function getStaticProps(context) {
+          
+      export async function getStaticProps(context) {
    // fetch data for a single meetup
 
    const meetupId = context.params.meetupId;
-   console.log(meetupId);
+
+   const client = await MongoClient.connect('mongodb+srv://bvp18061992:DGfa3uKmiU4SCt5m@cluster0.j5ygu.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0');
+   const db = client.db();
+
+   const meetupsCollection = db.collection('meetups');
+   const selectedMeetup = await meetupsCollection.findOne({_id: ObjectId(meetupId),});
+  
+   client.close();
 
    return {
       props: {
-         meetupData: {
-            image: 'https://png.pngtree.com/thumb_back/fh260/background/20210907/pngtree-business-daytime-conference-table-meeting-room-placement-photographs-and-pictures-image_816353.jpg',
-            id: meetupId,
-            title: 'First Meetup',
-            address: 'Some Street 5, Some City',
-            description: 'This is a first meetup'
-         }
+         meetupData:{
+            id: selectedMeetup._id.toString(),
+            title: selectedMeetup.title,
+            address: selectedMeetup.address,
+            image: selectedMeetup.image,
+            description: selectedMeetup.description,
+         } 
       }
    }
 }
